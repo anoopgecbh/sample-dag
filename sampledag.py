d9452767b84136a8369ee3ffa9c21dbbe28c9008
Anoop Singh <anoop.singh@auto-grid.com>
2021-07-09 17:43:08 +0530

    Create sampledag.py

---
+++ sampledag.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+#from airflow.operators.datalab import EventBatchOperator
+#from event_batch_gdsp import EventBatchGDSPOperator
+
+seven_days_ago = datetime.combine(datetime.today() - timedelta(7),
+                                datetime.min.time())
+
+default_args = {
+  'owner': 'Airflow',
+  'depends_on_past': False,
+  'start_date': seven_days_ago,
+  'email': ['airflow@airflow.com'],
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 1,
+  'retry_delay': timedelta(minutes=30),
+}
+dag = DAG(ls_example', description='ls_example',
+        schedule_interval='* * * * *', default_args=default_args)
+
+t0 = BashOperator(task_id='jup_run',
+                bash_command="ls -la"
+                dag=dag)
+
+t0
